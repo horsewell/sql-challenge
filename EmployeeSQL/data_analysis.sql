e0e66ff68e721da4b4557b16e22e13a15842570e
Tyson <tyson@horsewell.me>
2024-01-26 13:56:35 +0800

    Update data_analysis.sql
    adding additional items

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -36,3 +36,8 @@ JOIN departments AS d ON d.dept_no IN (SELECT dept_no FROM dept_emp AS de WHERE 
 
 -- 5. List the first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
+
+
+-- 6. List each employee in the Sales department, including their employee number, last name, and first name.
+-- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
