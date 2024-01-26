45b53d2453290112756cfdab11c4ff61f3cf47f0
Tyson <tyson@horsewell.me>
2024-01-26 14:04:56 +0800

    Update data_analysis.sql
    page 6

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -41,5 +41,16 @@ FROM employees AS e
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- 6. List each employee in the Sales department, including their employee number, last name, and first name.
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees AS e
+JOIN departments AS d ON d.dept_no IN (SELECT dept_no FROM dept_emp AS de WHERE de.emp_no = e.emp_no)
+WHERE d.dept_name = 'Sales';
+
 -- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+
 -- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+
