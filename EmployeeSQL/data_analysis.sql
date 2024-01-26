3b0bc789ff3f4eeb04ef67cd293d20687a360804
Tyson <tyson@horsewell.me>
2024-01-26 13:49:44 +0800

    Data Analysis SQL

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -1,13 +1,3 @@
-SELECT * FROM departments;
-SELECT * FROM titles;
-
-SELECT (SELECT COUNT(*) FROM employees) AS TOTAL_EMP, 
-		(SELECT COUNT(*) FROM salaries) AS TOTAL_SALARIES, 
-		(SELECT COUNT(*) FROM titles) AS TOTAL_TITLES, 
-		(SELECT COUNT(*) FROM dept_emp) AS TOTAL_EMP_IN_DEPTS,
-		(SELECT COUNT(*) FROM departments) AS TOTAL_DEPTS, 
-		(SELECT COUNT(*) FROM dept_manager) AS TOTAL_DEPT_MANAGERS;
-
 SELECT first_name, last_name, sex, hire_date, s.salary, d.dept_name
 FROM employees AS e
 JOIN salaries AS s ON e.emp_no = s.emp_no
